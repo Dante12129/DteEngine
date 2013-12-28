817686f34c8bdcc0efd6afc650a7cc1326b68145
Dante12129 <wd@cox.net>
2013-12-27 21:42:40 -0500

    Transparency Help
    Added a function to set the transparency of a drawable in SFML.

--- DteEngine/version.h
+++ DteEngine/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "29";
-	static const char MONTH[] = "11";
+	static const char DATE[] = "27";
+	static const char MONTH[] = "12";
 	static const char YEAR[] = "2013";
-	static const char UBUNTU_VERSION_STYLE[] =  "13.11";
+	static const char UBUNTU_VERSION_STYLE[] =  "13.12";
 	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 0;
-	static const long BUILD  = 78;
-	static const long REVISION  = 430;
+	static const long BUILD  = 79;
+	static const long REVISION  = 440;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 112;
-	#define RC_FILEVERSION 0,0,78,430
-	#define RC_FILEVERSION_STRING "0, 0, 78, 430\0"
-	static const char FULLVERSION_STRING [] = "0.0.78.430";
+	#define RC_FILEVERSION 0,0,79,440
+	#define RC_FILEVERSION_STRING "0, 0, 79, 440\0"
+	static const char FULLVERSION_STRING [] = "0.0.79.440";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 78;
+	static const long BUILD_HISTORY  = 79;
 	
 
 }
